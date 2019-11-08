bf8ec1ccf73db759784a66e611a378775a1ed5af
Justin Bull <bull.justin@gmail.com>
2019-11-07 21:59:19 -0800

    Add github fork ribbon to header

--- example/src/components/Header/index.jsx
+++ example/src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SVG from 'react-inlinesvg';
 import style from './index.styl';
 import logo from '~/assets/images/logo.svg';
+import githubCorner from '~/assets/images/github-corner.svg';
 
 class Header extends React.Component {
 
@@ -19,7 +20,7 @@ class Header extends React.Component {
           </p>
         </div>
         <a href="https://github.com/jabes/tofu" className={style.fork}>
-          <img src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub"/>
+          <SVG src={githubCorner} className={style.githubCorner}/>
         </a>
       </header>
     );
